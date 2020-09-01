2ad1db0114cd58d8acd7f4f06deab8f1aee727d7
MarcoBroto <msoto9940@gmail.com>
2020-09-01 11:31:41 -0600

    Improved regular expression and fixed output file bug. Lab is complete.

--- wordCount.py
+++ wordCount.py
@@ -1,6 +1,7 @@
 #! python2.7
 import sys
 import re
+import os
 
 def countWordsInFile(inputFileName, outputFileName):
     # Find and load input and output files
@@ -10,23 +11,23 @@ def countWordsInFile(inputFileName, outputFileName):
     # Parse out whitespace and punctuation
     words = dict()
     for line in inputFile:
-        for word in re.split('[\W\s]*', line):
+        for word in re.split('\W', line.lower()):
             words[word] = words[word]+1 if word in words else 1
     del words[''] # Remove empty string from word dictionary
 
     # Sort words by ascending frequency and write stats to output file
-    for k, v in sorted(words.items(), key=lambda x: x[1]): outputFile.write('%s %d\n' % (k, v))
+    for k, v in sorted(words.items()): outputFile.write('%s %d\n' % (k, v))
 
 def main():
     if len(sys.argv) < 2:
         print('File arguments not passed in')
         return
     
-    # Assign input and ouput file names from CL arguments
+    # Assign input and output file names from CL arguments
     file_in = sys.argv[1]
     file_out = sys.argv[2] if len(sys.argv) >= 3 else 'output.txt'
 
     try: countWordsInFile(file_in, file_out)
-    except IOError as err: print(err)
+    except IOError as err: print(err.with_traceback())
 
 if __name__ == "__main__": main()
