688cb61bd5e886633c7a0900a4542b5f922b473f
MarcoBroto <msoto9940@gmail.com>
2020-09-01 18:24:14 -0600

    Shortened file io code

--- wordCount.py
+++ wordCount.py
@@ -3,19 +3,17 @@ import sys
 import re
 
 def countWordsInFile(inputFileName, outputFileName):
-    # Load input and output files
-    inputFile = open(inputFileName, 'r')
-    outputFile = open(outputFileName, 'w')
-    
-    # Parse out whitespace and punctuation
     words = dict()
-    for line in inputFile:
-        for word in re.split('\W', line.lower()):
-            words[word] = words[word]+1 if word in words else 1
-    del words[''] # Remove empty string from word dictionary
+    with open(inputFileName, 'r') as inputFile:
+        # Parse out whitespace and punctuation
+        for line in inputFile:
+            for word in re.split('\W', line.lower()):
+                words[word] = words[word]+1 if word in words else 1
+        del words[''] # Remove empty string from word dictionary
 
     # Alphabetically sort dictionary by keys and write keys and values to output file
-    for k, v in sorted(words.items()): outputFile.write('%s %d\n' % (k, v))
+    with open(outputFileName, 'w') as outputFile:
+        for k, v in sorted(words.items()): outputFile.write('%s %d\n' % (k, v))
 
 def main():
     if len(sys.argv) < 2:
@@ -27,6 +25,6 @@ def main():
     file_out = sys.argv[2] if len(sys.argv) >= 3 else 'output.txt'
 
     try: countWordsInFile(file_in, file_out)
-    except IOError as err: print(err.with_traceback())
+    except IOError as err: print(err)
 
 if __name__ == "__main__": main()
